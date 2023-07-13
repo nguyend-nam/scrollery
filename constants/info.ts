6825c1ca0c617ace70b7cce6a80d238275388c8e
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2023-07-13 13:28:22 +0700

    fix: fix ui landing page on safari (#18)

---
+++ constants/info.ts
@@ -0,0 +1,59 @@
+export const featuresMedias = [
+  {
+    id: 1,
+    img: "/feature-1-bg.jpg",
+    url: "https://unsplash.com/photos/-WBYxmW4yuw",
+    icon: "fluent:hand-draw-28-regular",
+  },
+  {
+    id: 2,
+    img: "/feature-2-bg.jpg",
+    url: "https://unsplash.com/photos/RSvJQ-IP_pk",
+    icon: "fluent:target-arrow-20-regular",
+  },
+  {
+    id: 3,
+    img: "/feature-3-bg.jpg",
+    url: "https://unsplash.com/photos/KE0nC8-58MQ",
+    icon: "fluent:code-text-edit-20-regular",
+  },
+];
+
+export const featuresSummary = [
+  { feature: "enhanced", action: "visual experience" },
+  { feature: "interactive", action: "image animations" },
+  { feature: "simple", action: "integration" },
+];
+
+export const keyFeatures = [
+  {
+    title: "Visual Experience Enhancement",
+    description:
+      "Whether you're building a portfolio website, a creative landing page, or an interactive storytelling platform, Scrollery empowers you to effortlessly bring your designs to life with captivating scroll animations.",
+  },
+  {
+    title: "Simple Integration",
+    description:
+      "Scrollery offers a user-friendly and straightforward integration process, allowing you to easily animate images with minimal setup. Our library provides a set of intuitive components that seamlessly work with your existing codebase, enabling you to focus on creating visually impactful scroll animations without unnecessary complexity.",
+  },
+  {
+    title: "Customization and Flexibility",
+    description:
+      "Scrollery provides a wide range of customization options, empowering you to tailor the image animations to suit your project's specific requirements. Adjust animation timings, effects, transitions, and more to create personalized and immersive experiences that perfectly align with your design vision.",
+  },
+];
+
+export const teamMems = [
+  {
+    fullName: "Nam Nguyen Dinh",
+    img: "/namnd.jpg",
+    gitHub: "https://github.com/nguyend-nam",
+    linkedIn: "https://www.linkedin.com/in/nguyendinhnam0320/",
+  },
+  {
+    fullName: "Lap Nguyen Ngo",
+    img: "/lapnn.jpeg",
+    gitHub: "https://github.com/ngolapnguyen",
+    linkedIn: "https://www.linkedin.com/in/ngo-lap-nguyen/",
+  },
+];
